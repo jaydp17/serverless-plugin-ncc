b1f4b6cb09d41be52877b91a551f24b0637b4009
Jaydeep Solanki <jaydp17@gmail.com>
2018-11-29 23:26:32 +0530

    add eslint & fix a few issues
    + format a few files

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@jaydp17'],
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # serverless-plugin-ncc
+
 A serverless plugin to use @zeit/ncc for bundling
