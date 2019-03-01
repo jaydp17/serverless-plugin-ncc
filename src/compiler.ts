69a7629459546fc7f51159ce247dae5cd2c0d70f
Dirk de Visser <dirkdev98@gmail.com>
2019-03-01 09:39:48 +0100

    Add handling NCC custom options and saving assets.
    It's now possible to pass options from serverless.yml to ncc
    
    All assets outputted from ncc are now also written to the zip output.

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,6 +1,5 @@
 import zeitNcc from '@zeit/ncc';
 
-export default async function compile({ inputFilePath }: { inputFilePath: string }) {
-  const { code } = await zeitNcc(inputFilePath, { minify: false });
-  return code;
+export default async function compile({ inputFilePath }: { inputFilePath: string }, opts: any) {
+  return zeitNcc(inputFilePath, { minify: false, ...opts });
 }
