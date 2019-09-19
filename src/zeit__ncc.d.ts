a58be9daa51244fdf2d612767f9b6ea21790a024
Jaydeep Solanki <jaydp17@gmail.com>
2019-09-19 19:22:12 +0530

    types: add permissions property to Asset type

--- src/zeit__ncc.d.ts
+++ src/zeit__ncc.d.ts
@@ -1,5 +1,5 @@
 declare module '@zeit/ncc' {
-  type Asset = { source: Buffer; };
+  type Asset = { source: Buffer; permissions: number; };
   export type CompiledOutput = { code: string; assets?: { [key: string]: Asset }; map?: string };
   type CompilerOptions = { minify?: boolean; externals?: string[]; sourceMap?: boolean };
   export default function(inputFilePath: string, options: CompilerOptions): CompiledOutput;
